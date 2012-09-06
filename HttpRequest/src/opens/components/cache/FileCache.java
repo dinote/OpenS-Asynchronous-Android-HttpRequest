872580d92da406c85bb36ae248544ba8e2658da1
Leonardo <leonardoxh@gmail.com>
2012-09-06 19:24:58 -0300

    Pre implementation of caching and BitmapLoader

--- HttpRequest/src/opens/components/cache/FileCache.java
+++ HttpRequest/src/opens/components/cache/FileCache.java
@@ -1,104 +1,45 @@
 package opens.components.cache;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+import java.io.InputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
-import opens.components.cache.serializers.CacheSerializer;
+import opens.components.cache.serializers.BitmapSerializer;
 
 import android.content.Context;
 
-/**
- * Permanent cache using the Android cache directory for storage.
- * Use this when you want permanent storage. It is an excellent 
- * image cache.
- * 
- * Slower read and write than MemoryCache, but permanently stores data.
- * 
- * @author Vatroslav Dino Matijas
- *
- */
-public class FileCache  extends Cache{
+public class FileCache {
 			
 	private String rootDir; 	
+	private static final String TAG = "/opens/"
 	
-	public FileCache(Context context, String tag) {				
-		this.rootDir = createRootDir(context, tag);		
+	public FileCache(Context context) {				
+		this.rootDir = "/" + createRootDir(context);		
 	}
 	
-	protected String createRootDir(Context context, String tag) {		
-		Context appContext = context.getApplicationContext();
-		
-		File cacheDir = appContext.getCacheDir();		
-		String rootDir = cacheDir.getAbsolutePath();
-		
-		rootDir += "/opens/" + tag; 
+	protected final String createRootDir(Context context) {		
+		String rootDir = context.getCacheDir().getAbsolutePath() + FileCache.TAG; 
 		File file = new File(rootDir);		
 		file.mkdirs();
-		
 		return rootDir;
 	}
 	
-	synchronized public void remove(String key) {
-		File file = new File(rootDir + "/" + key);
-		if (file.exists()) {
-			file.delete();
-		}
+	public synchronized boolean remove(String key) {
+		return new File(this.rootDir+key).delete();
 	}
-
-	@Override
-	synchronized public Object get(String key, CacheSerializer serializer) {
-		BufferedInputStream in = null;	
-		File file = new File(rootDir + "/" + key);
-		try {
-			//TODO do expiration check. check file.lastModified()
-			if (file.exists()) {
-				in = new BufferedInputStream(new FileInputStream(file));
-				return serializer.deserializeFromInputStream(in);
-			}
-		} catch (FileNotFoundException e) {			
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (in != null) {
-				try {
-					in.close();
-				} catch (IOException e) {
-					// OK to ignore						
-				}
-			}
-		}
+	
+	public synchronized Object get(String key) {
+		File file = new File(this.rootDir+key);
+		if(file.exists())
+			BitmapSerializer.deserialize(file.getAbsolutePath());
 		return null;
 	}
 
-	@Override
-	synchronized public void put(String key, Object value, CacheSerializer serializer) {
-		File file = new File(rootDir + "/" + key);
-		BufferedOutputStream out = null;
-		try {
-			file.createNewFile();
+	public synchronized void put(String key, InputStream content) {
+		File file = new File(this.rootDir+key);
+		if(!file.exists()) {
 			file.deleteOnExit();
-			out = new BufferedOutputStream(new FileOutputStream(file));
-			serializer.serializeToOutputStream(out, value);
-		} catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-        	if (out != null) {
-				try {
-					out.close();
-				} catch (IOException e) {
-					// OK to ignore						
-				}
-			}
-        }
-		
+			BitmapSerializer.serialize(content, file.getAbsolutePath());
+		}
 	}
 
 	
--- HttpRequest/src/opens/components/cache/serializers/BitmapSerializer.java
+++ HttpRequest/src/opens/components/cache/serializers/BitmapSerializer.java
@@ -1,31 +1,57 @@
 package opens.components.cache.serializers;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.widget.ImageView;
 
-public class BitmapSerializer implements CacheSerializer {
+public final class BitmapSerializer {
 	
-	private static BitmapSerializer instance;
+	private BitmapSerializer() { }
 	
-	public static BitmapSerializer instance() {
-		if (instance == null) {
-			instance = new BitmapSerializer();
+	private static final int MAX_BUFFER_SIZE = 8192;
+	
+	public static void serialize(InputStream input, String destiny) {
+		try {
+			BufferedInputStream buffer = new BufferedInputStream(input, BitmapSerializer.MAX_BUFFER_SIZE);
+			FileOutputStream exitFile = new FileOutputStream(destiny);
+			int currentByte = 0;
+			byte[] buffered = new byte[BitmapSerializer.MAX_BUFFER_SIZE];
+			while((currentByte = buffer.read(buffered, 0, BitmapSerializer.MAX_BUFFER_SIZE)) != -1)
+				exitFile.write(buffered, 0, currentByte);
+			input.close();
+			input = null;
+			exitFile.flush();
+			exitFile.close();
+			exitFile = null;
 		}
-		return instance;
+		catch(IOException e) { }
+		catch(OutOfMemoryError ex) { }
 	}
 	
-	//CacheSerializer	
-	public Object deserializeFromInputStream(InputStream is) throws IOException{
-		return BitmapFactory.decodeStream(is);
+	public static Bitmap deserialize() {
+		try {
+			BitmapFactory.Options opt = new BitmapFactory.Options();
+			opt.inPurgeable = true;
+			opt.inJustDecodeBounds = true;
+			BitmapFactory.decodeFile(input, opt);
+			int scale = 1;
+			final int requiredSize = 250;
+			while(opt.outWidth/scale/2>=requiredSize && opt.outHeight/scale/2>=requiredSize)
+				scale*=2;
+			opt.inJustDecodeBounds = false;
+			opt.inSampleSize = scale;
+			return BitmapFactory.decodeFile(input, opt);
+		}
+		catch(OutOfMemoryError e) { }
 	}
 	
-	public void serializeToOutputStream(OutputStream os, Object object) throws IOException{
-		Bitmap bitmap = (Bitmap)object;
-		bitmap.compress(Bitmap.CompressFormat.PNG, 100, os);		
+	public static Bitmap display(ImageView image, String file) {
+		return new BitmapLoader(image).execute(file);
 	}
 }
